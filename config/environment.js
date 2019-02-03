02f1b59a41fc3e6f3b8a7228fd6864b06ed213a1
raytiley <raytiley@gmail.com>
2019-02-03 08:02:09 -0500

    Setup some stuff

--- config/environment.js
+++ config/environment.js
@@ -44,8 +44,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
-  }
+  ENV.rootURL = '/journal';
+  ENV.locationType = 'hash';
+}
 
   return ENV;
 };
